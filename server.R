4f2b58b64e2ddf10a64f7791bb0cf70ce0992756
micheledemeo <micheledemeo@gmail.com>
2015-01-05 17:48:41 +0100

    selectInput with multiple=T added. ok checks with where condition in data.table. first check (not ok) with fluid pages. started to study more tabs in layout.

--- server.R
+++ server.R
@@ -3,21 +3,38 @@ source( paste(getwd(), "source/ini_fun.R", sep="/") )
 source( pastedir(getwd() , "source/ini_data.R") )
 
 var=d[,unique(var)]
-strato=d[giorni_mare>0,unique(id_strato)]
+strato=d[,unique(id_strato)]
+regione=d[,unique(regione)]
+codsis=d[,unique(codsis199)]
+codlft=d[,unique(codlft199)]
 
 shinyServer(function(input, output) {
   
   input_var=reactive({ input$var  })
+  input_codsis=reactive({ input$codsis  })
+  input_codlft=reactive({ input$codlft  })
   input_strato=reactive({ input$strato  })
   input_check_gio=reactive({ input$check_gio  })
   
-  output$var=renderUI({  checkboxGroupInput("var", label = "Choose a variable to analyze", choices =var , selected=var) })
-  output$strato=renderUI({  selectInput("strato", label = "Choose a strata to analyze", choices =strato, selected = strato[1] ) })
+  #output$var=renderUI({  checkboxGroupInput("var", label = "Choose a variable to analyze", choices =var , selected=var) })
+  output$var=renderUI({  selectInput("var", label = "Scegli variabili di costo:", choices =var, selected = var, multiple = T ) })  
+  output$codsis=renderUI({  selectInput("codsis", label = "Filtra sistema di pesca:", choices =codsis, selected = codsis, multiple = T ) })
+  output$codlft=renderUI({  selectInput("codlft", label = "Filtra classe lft:", choices =codlft, selected = codlft, multiple = T ) })
+  
+  output$strato=renderUI({  selectInput("strato", label = "Filtra strato:", choices =strato, selected = NULL, multiple = T ) })
   
   output$boxplot = renderPlot({
     
-    d[giorni_mare>(input_check_gio()-1) & id_strato==input_strato() & var==input_var() ,ggplot(.SD, aes(x= var,y=value)) +geom_boxplot(outlier.size=3 ,outlier.colour="red", fill="grey",colour = "blue") + xlab("")]
-    
+    if (  !is.null( input_strato() )  ) {
+      
+      d[giorni_mare>(input_check_gio()-1) & id_strato %in% input_strato() & var %in% input_var() ,ggplot(.SD, aes(x= var,y=value)) +geom_boxplot(outlier.size=3 ,outlier.colour="red", fill="grey",colour = "blue") + xlab("") ]
+  
+    } else {
+      
+      d[giorni_mare>(input_check_gio()-1) & var %in% input_var() & codsis199 %in% input_codsis() & codlft199 %in% input_codlft(), ggplot(.SD, aes(x= var,y=value)) +geom_boxplot(outlier.size=3 ,outlier.colour="red", fill="grey",colour = "blue") + xlab("")]
+        
+    }
+      
     
   })
 })
--- ui.R
+++ ui.R
@@ -3,17 +3,45 @@ shinyUI(fluidPage(
   
   titlePanel("Outliers control with boxplot"),
   
+    
   sidebarLayout(
     
-    sidebarPanel(
+      sidebarPanel(
+        
+        fluidRow(
+          column(12,
+                 h5("Test page:"),
+                 actionButton("action", label = "Action"),
+                 br())
+          
+        ),
+        
+        br(),
       
       uiOutput("var")
+      ,uiOutput("codsis")
+      ,uiOutput("codlft")
       ,uiOutput("strato")
       ,checkboxInput(inputId = "check_gio",label = "Filtra con giorni_mare>0", value=T)
       
       
     ),
+
     
-    mainPanel( plotOutput("boxplot") )
+    mainPanel( plotOutput("boxplot") ),
+    
+    
+  ),
+  
+  fluidRow(
+  
+    column(1,
+           h3("Test page:"),
+           actionButton("action", label = "Action"),
+           br(),
+           br(), 
+           actionButton("action2", label = "Action2"))
+ 
   )
 ))
+
