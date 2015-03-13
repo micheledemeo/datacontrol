1f288d409aad7e05414cf3bf44cc2e89789b8ac1
micheledemeo <micheledemeo@gmail.com>
2015-03-13 16:07:52 +0100

    added http instruction to start file

--- run_shiny.cmd
+++ run_shiny.cmd
@@ -1,3 +1,1 @@
-"C:\Program Files\R\R-3.1.1\bin\x64\Rscript.exe" --no-save --no-restore -e "shiny::runApp('C:/Users/mdemeo/Documents/000/datacontrol')"
-
-pause
+"C:\Program Files\R\R-3.1.1\bin\x64\R.exe" -e "shiny::runApp('C:/Users/mdemeo/Documents/000/datacontrol', port = 12345, launch.browser=TRUE)"
