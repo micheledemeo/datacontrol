998573c750d9f26b0289674e574c36f4b77af701
micheledemeo <micheledemeo@gmail.com>
2015-07-11 11:01:14 +0200

    response_rate added in tab sample rate

--- source/sample_rate.R
+++ source/sample_rate.R
@@ -3,7 +3,8 @@ output$keyby_sample_data=renderUI({  selectInput("keyby_sample_rate", label = NU
 sample_data_react=reactive({
   
   sample_rate[,list(achieved_sample_rate=round(100*(sum(sample_achieved_units)/sum(universe_units)),2),
-                  starting_sample_rate=round(100*sum(sample_units)/sum(universe_units),2)
+                  starting_sample_rate=round(100*sum(sample_units)/sum(universe_units),2),
+                  response_rate=round(100*sum(sample_achieved_units)/sum(sample_units),2)
                   ),
            by=eval(input_keyby_sample_rate())]
   
